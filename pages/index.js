009a16ac1b37fbab6cf6f249c87ec4fe4bad66be
Amuthan Mannan <amuthan.m@thoughtworks.com>
2020-07-15 20:19:38 +0530

    Hope will work in deploy

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,32 @@
 import Head from 'next/head'
+import HeroPost from './hero-post'
+import {getAllPostsForHome} from '../prismic-configuration'
 
-export default function BlogHome () {
+export default function BlogHome({preview,allPosts}) {
+  const heroPost = allPosts[0].node
+  const morePosts = allPosts.slice(1)
+  
   return (
     <>
-      <Head>
-        <title>Student Journalist Council</title>
-      </Head>
-      Hi i am aj
+        <Head>
+          <title>Student Journalist Council</title>
+        </Head>
+          {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              coverImage={heroPost.coverimage}
+            />
+          )}
+            
     </>
   )
+}
+
+
+export async function getServerSideProps({ preview = false, previewData }) {
+  const allPosts = await getAllPostsForHome(previewData)
+  return {
+    props: { preview, allPosts },
+  }
+  
 }
