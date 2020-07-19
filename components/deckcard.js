1c6a96f0c0615e71ab8bac575a1b4329fe6375bf
sjcgct <64430846+sjcgct@users.noreply.github.com>
2020-07-19 13:00:54 +0530

    ajai rever back

--- components/deckcard.js
+++ components/deckcard.js
@@ -4,7 +4,7 @@ export default function DeckCard ({ title, imgurl }) {
     <div className='card mb-4 col-md-4 border-0'>
       <div className='card-body '>
         <h5 className='card-title'>{title}</h5>
-        <p className='card-text'>{excerpt}</p>
+        <p className='card-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni eaque tenetur suscipit repellat, sapiente cumque quis aspernatur fugit voluptatum nihil, distinctio earum? Animi consectetur tempora ut nostrum explicabo a ad!</p>
       </div>
       <div className='card-footer'>
         <small className='text-danger'>Read</small>
