0931605cf0090be1bf3303b266740e863d795bf9
ajboi <67203096+ajboi@users.noreply.github.com>
2020-11-17 13:43:05 +0530

    fixed hopefully

--- components/profileDeckCard.js
+++ components/profileDeckCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 
-export default function ProfilePostCard ({ title, about, imgurl, imgalt, id, sub_head }) {
+export default function ProfilePostCard ({ title, about, imgurl, imgalt, id, sub_head, category }) {
   var redirect = {
     pathname: `/blogs/${category}/[slug]`,
     state: { slug: id }
