22695f41015e5f537bdaf69d99b3dd227557e10b
ajboi <67203096+ajboi@users.noreply.github.com>
2020-11-26 20:02:20 +0530

    team page slicing done

--- components/YearPagination.js
+++ components/YearPagination.js
@@ -1,50 +1,50 @@
-const { Component } = require("react");
+const { Component } = require('react')
 
-class YearPagination extends Component{
-   constructor(props){
-       super(props);
-       var next_year=this.getNext(props.year,props.years);
-       var prev_year=this.getPrev(props.year,props.years);
-       var hide_next=false;
-       var hide_prev=false;
-       if(next_year===null) {
-          hide_next=true;
-       }
-       if(prev_year===null) {
-          hide_prev=true;
-     }
-
-       this.state={
-        year:props.year,
-        years:props.years,
-        next_year:next_year,
-        prev_year:prev_year,
-        hide_next:hide_next,
-        hide_prev:hide_prev
-       }
-   }
-   
-   getNext(current_year,years){
-      if(years[years.length-1]===current_year){
-          return null;
-      }
-      for(var i=0;i<years.length;i++){
-          if(years[i]===current_year) return years[i+1];
-      }
-   }
-
-   getPrev(current_year,years){
-    if(years[0]===current_year){
-        return null;
+class YearPagination extends Component {
+  constructor (props) {
+    super(props)
+    var next_year = this.getNext(props.year, props.years)
+    var prev_year = this.getPrev(props.year, props.years)
+    var hide_next = false
+    var hide_prev = false
+    if (next_year === null) {
+      hide_next = true
     }
-    for(var i=1;i<years.length;i++){
-        if(years[i]===current_year) return years[i-1];
+    if (prev_year === null) {
+      hide_prev = true
     }
- }
 
-   render(){
-       return(
-        <div className='row'>
+    this.state = {
+      year: props.year,
+      years: props.years,
+      next_year: next_year,
+      prev_year: prev_year,
+      hide_next: hide_next,
+      hide_prev: hide_prev
+    }
+  }
+
+  getNext (current_year, years) {
+    if (years[years.length - 1] === current_year) {
+      return null
+    }
+    for (var i = 0; i < years.length; i++) {
+      if (years[i] === current_year) return years[i + 1]
+    }
+  }
+
+  getPrev (current_year, years) {
+    if (years[0] === current_year) {
+      return null
+    }
+    for (var i = 1; i < years.length; i++) {
+      if (years[i] === current_year) return years[i - 1]
+    }
+  }
+
+  render () {
+    return (
+      <div className='row'>
         <div className='mx-auto'>
           <a hidden={this.state.hide_prev} href={`/team/${this.state.prev_year}`} className='about-pagination-previous'>&lt;</a>
           <span>{this.state.year}</span>
@@ -73,11 +73,9 @@ class YearPagination extends Component{
                         `}
           </style>
         </div>
-        </div>
-       )
-   }
-
+      </div>
+    )
+  }
 }
 
-export default YearPagination;
-
+export default YearPagination
--- pages/team/[year].js
+++ pages/team/[year].js
@@ -1,47 +1,66 @@
 import React, { Component } from 'react'
 import Layout from '../../components/Layout'
 import YearPagination from '../../components/YearPagination'
-import {getAllTeams, getByYear} from '../../prismic-configuration'
-import {RichText} from 'prismic-reactjs';
+import { getAllTeams, getByYear } from '../../prismic-configuration'
+import { RichText } from 'prismic-reactjs'
 
 class About extends Component {
-  constructor(props){
-    super(props);
-    var total_pages=props.years.length;
-    this.state={
-        years:props.years,
-        year:props.current_year,
-        total_pages:total_pages,
-        post:props.post
+  constructor (props) {
+    super(props)
+    var total_pages = props.years.length
+    this.state = {
+      years: props.years,
+      year: props.current_year,
+      total_pages: total_pages,
+      post: props.post
     }
   }
 
   render () {
-    var post=this.state.post;
-    var htmlcontent=[];
-    post.body.forEach((value)=>{
-      try{
-        htmlcontent.push(<br></br>);
-        var subTeam=value.primary.subTeam;
-        var members=value.primary.memberList;
-        var memberList=members[0].text.split(",");
-        htmlcontent.push(<h1 className='blog-post-title'>{RichText.asText(subTeam)}</h1>);
-        memberList.forEach(member=>{
-          htmlcontent.push(<blockquote> {member} </blockquote>);
-        })
-      }
-      catch(e){}
+    var post = this.state.post
+    // var htmlcontent = []
+    // post.body.forEach((value) => {
+    //   try {
+    //     htmlcontent.push(<br></br>)
+    //     var subTeam = value.primary.subTeam
+    //     var members = value.primary.memberList
+    //     var memberList = members[0].text.split(',')
+    //     htmlcontent.push(<h1 className='blog-post-title'>{RichText.asText(subTeam)}</h1>)
+    //     memberList.forEach(member => {
+    //       htmlcontent.push(<blockquote> {member} </blockquote>)
+    //     })
+    //   } catch (e) {}
+    // })
+
+    var htmlcontent = []
+    post.body.forEach((value) => {
+      try {
+        htmlcontent.push(RichText.render(value.primary.subTeam))
+        htmlcontent.push(RichText.render(value.primary.memberList))
+      } catch (e) {}
     })
-    
+    // var htmltitle
+    // htmltitle = post.body.map(slice => {
+    //   if (slice.type === 'sub-team') {
+    //     return RichText.render(slice.primary.subTeam)
+    //   }
+    // })
+    // var htmlcontent
+    // htmlcontent = post.body.map(slice => {
+    //   if (slice.type === 'sub-team') {
+    //     return RichText.render(slice.primary.memberList)
+    //   }
+    // })
+
     return (
       <Layout>
 
         <div className='blog-container'>
           {htmlcontent}
-        </div>  
-         
+        </div>
+
         <YearPagination year={this.state.year} years={this.state.years}></YearPagination>
-          
+
       </Layout>
 
     )
@@ -50,19 +69,19 @@ class About extends Component {
 
 export default About
 
-export async function getServerSideProps ({ params}) {
-  const current_year=params.year;
-  const teams = await getAllTeams();
-  var edges=teams.edges;
-  var years=[]
-  for (var i=0;i<edges.length;i++){
-  years[i]=edges[i].node.year;
+export async function getServerSideProps ({ params }) {
+  const current_year = params.year
+  const teams = await getAllTeams()
+  var edges = teams.edges
+  var years = []
+  for (var i = 0; i < edges.length; i++) {
+    years[i] = edges[i].node.year
   }
 
-  const fetchedpost = await getByYear(current_year);
-  const post = fetchedpost[0].node;
-  
+  const fetchedpost = await getByYear(current_year)
+  const post = fetchedpost[0].node
+
   return {
-    props: { years,current_year,post}
+    props: { years, current_year, post }
   }
 }
